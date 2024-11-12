08871660d6861444849d67d7e0d550ba3f3a1c37
Amol Shirke <shirkeamy@gmail.com>
2024-11-12 07:43:56 +0530

    Session 7 - React Coponents

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,43 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
 
 function App() {
 
   const welcome: string = "Welcome to React TS";
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>{welcome}</h2>
-        <p>
-          {1 + 1}
+    <div>
+      <div className="App">
+        <Header />
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>{welcome}</h2>
+          <p>
+            {1 + 1}
             <br />
-          {
-            2 + 2
-          }
-        </p>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+            {
+              2 + 2
+            }
+          </p>
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+
+        <Footer />
+
+      </div>
     </div>
   );
 }
---
+++ src/Components/Header.tsx
@@ -0,0 +1,11 @@
+function Header() {
+    return (
+        <div>
+            <h1>Header</h1>
+            <p>
+                This is header component...
+            </p>
+        </div>
+    )
+}
+export default Header;
